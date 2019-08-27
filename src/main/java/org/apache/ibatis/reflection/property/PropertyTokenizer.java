88e8700d621a98933c90e5dcc1ecbe3438e65ee4
ooooo <297872913@qq.com>
2019-08-27 17:18:36 +0800

    解析PropertyTokenizer -->属性表达式的解析类 例如：orders[O].items[O].name

--- src/main/java/org/apache/ibatis/reflection/property/PropertyTokenizer.java
+++ src/main/java/org/apache/ibatis/reflection/property/PropertyTokenizer.java
@@ -19,26 +19,46 @@ import java.util.Iterator;
 
 /**
  * @author Clinton Begin
+ *
+ * 这个类用来解析xml中标签<result property=”orders[0].items[0].name” column=”item1”/>
+ * 中的属性orders[O].items[O].name 表达式,,
+ * 注意这个类试实现了Iterator，所以调用next()能处理多层嵌套表达式
  */
 public class PropertyTokenizer implements Iterator<PropertyTokenizer> {
+
+  // 当前表达式的名称
   private String name;
+  // 当前表达式的索引名
   private final String indexedName;
+  // 索引
   private String index;
+  // 子表达式
   private final String children;
 
   public PropertyTokenizer(String fullname) {
+    /*
+    * 传入fullname参数，例如 orders[O].items[O].name
+    * */
+    // 获得'.'的索引
     int delim = fullname.indexOf('.');
     if (delim > -1) {
+      // 存在'.'，获得name -=> orders[0]
       name = fullname.substring(0, delim);
+      // 获得children --> items[O].name
       children = fullname.substring(delim + 1);
     } else {
+      // 不存在'.'，name就是fullname，而children不存在
       name = fullname;
       children = null;
     }
+    // indexedName --> orders[0]
     indexedName = name;
+    // 获得'['的索引
     delim = name.indexOf('[');
     if (delim > -1) {
+      // 存在'[', 获得index --> 0
       index = name.substring(delim + 1, name.length() - 1);
+      // 获得name --> orders
       name = name.substring(0, delim);
     }
   }
@@ -66,6 +86,7 @@ public class PropertyTokenizer implements Iterator<PropertyTokenizer> {
 
   @Override
   public PropertyTokenizer next() {
+    // 这里传入的children (子表达式)
     return new PropertyTokenizer(children);
   }
 
